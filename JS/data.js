3d4f8e436c816e5b2668eb195c8a65850cb8d6bf
Aptech <Aptech@gamil.com>
2022-12-02 09:31:31 +0500

    we are sending files to cloud

---
+++ JS/data.js
@@ -0,0 +1,8 @@
+var Products = [ 
+{"ID":0 ,"Name":"","Category":"","Price":"","Image":"noimage.webp","Specification":""},
+{"ID":1 ,"Name":"Visachi","Category":"Shopulder Bag","Price":"$100","Image":"https://www.whatmobile.com.pk/admin/images/Vivo/VivoY55-b.jpg","Specification":"Two Zipper Bag"},
+{"ID":2 ,"Name":"LV","Category":"Cross Body","Price":"$500","Image":"https://www.whatmobile.com.pk/admin/images/Oppo/OppoReno6Pro-b.jpg","Specification":"Single Pcket multi shaded"},
+{"ID":3 ,"Name":"Gucci","Category":"Hand Bag","Price":"$150","Image":"https://www.whatmobile.com.pk/admin/images/Qmobile/Z9Plusb.jpg","Specification":"Medium size bag"},
+{"ID":4 ,"Name":"MK","Category":"Bag Pack","Price":"$1600","Image":"https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyS22Ultra-b.jpg","Specification":"Small Bag"},
+{"ID":5 ,"Name":"J.","Category":"Shoulder Bag","Price":"$1500","Image":"https://www.whatmobile.com.pk/admin/images/Apple/AppleiPhone13ProMax-b.jpg","Specification":"Medium size Bag"},
+]
