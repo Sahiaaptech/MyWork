98ce1e8f6160c907d12b3d5c812127ac830df5fa
ashtarshigri <ashtar.shigri.aptech55@gmail.com>
2022-12-05 09:52:53 +0500

    I have added new data2.js file in folder JS

---
+++ JS/data2.js
@@ -0,0 +1,9 @@
+var Products = [ 
+    {"ID":0 ,"Name":"","Category":"","Price":"","Image":"noimage.webp","Specification":""},
+    {"ID":1 ,"Name":"Ashtar","Category":"Data Management","Price":"$100","Image":"https://www.whatmobile.com.pk/admin/images/Vivo/VivoY55-b.jpg","Specification":"Two Zipper Bag"},
+    {"ID":2 ,"Name":"Hamza","Category":"Data Management","Price":"$500","Image":"https://www.whatmobile.com.pk/admin/images/Oppo/OppoReno6Pro-b.jpg","Specification":"Single Pcket multi shaded"},
+    {"ID":3 ,"Name":"Abdullah","Category":"Data Management","Price":"$150","Image":"https://www.whatmobile.com.pk/admin/images/Qmobile/Z9Plusb.jpg","Specification":"Medium size bag"},
+    {"ID":4 ,"Name":"Ahmed","Category":"Data Management","Price":"$1600","Image":"https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyS22Ultra-b.jpg","Specification":"Small Bag"},
+    {"ID":5 ,"Name":"Obaid","Category":"Data Management","Price":"$1500","Image":"https://www.whatmobile.com.pk/admin/images/Apple/AppleiPhone13ProMax-b.jpg","Specification":"Medium size Bag"},
+    ]
+    
